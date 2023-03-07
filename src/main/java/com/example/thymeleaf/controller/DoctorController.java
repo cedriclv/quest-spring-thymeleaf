64656dd4808b65ae0fe8a3f64e80f88cfd6696be
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-03-07 13:20:55 +0100

    first commit

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -11,18 +11,9 @@ public class DoctorController {
     @GetMapping("/doctor/")
     public String doctor(@RequestParam(required = false, defaultValue = "0") int number,
             @RequestParam(required = false, defaultValue = "John Smith") String name, Model model) {
-        // model.addAttribute("name", "toto");
+
         model.addAttribute("doctorname", name);
         model.addAttribute("doctornumber", number);
         return "doctor";
     }
 }
-// Ton projet affiche bien le nom et le numéro du docteur correspondant aux
-// liens
-
-// Si aucun nom ni numéro n'est renseigné, l'affichage doit être "John Smith" et
-// 0
-
-// Les templates index.html et doctor.html doivent appeler les deux fragments
-// fragments/header.html
-// et fragments/footer.html
